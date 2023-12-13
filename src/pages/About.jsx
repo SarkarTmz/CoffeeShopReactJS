2768f59866493e0e5d64a76e1e08f18720a3855f
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2023-12-14 00:29:13 +0545

    Updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,47 +1,19 @@
 import React from 'react'
+import '../styles/About.css'
+
+import shop1 from '../images/shop1.jpg'
+import shop2 from '../images/shop2.jpg'
+import shop3 from '../images/shop3.jpg'
 
 const About = () => {
   return (
     <div className='about'>
       <p>
-      Welcome to Mitho Coffee - Where Coffee Meets Perfection!
-
-      At Mitho Coffee, we are passionate about crafting the perfect cup of coffee. We believe that every coffee lover deserves an exceptional coffee experience, and that's exactly what we offer. Step into our cozy coffee haven and embark on a journey of rich flavors, warm ambiance, and friendly service.
-
-      The Art of Coffee
-
-      We take our coffee seriously. Our skilled baristas are artists in their own right, expertly brewing each cup to perfection. Whether you prefer the bold intensity of an espresso or the smooth comfort of a latte, we've got you covered. Our coffee menu is a symphony of flavors, featuring single-origin beans sourced from around the world and roasted to perfection in-house.
-
-      A World of Variety
-
-      At Mitho Coffee, we celebrate diversity. Our menu offers a wide range of coffee styles, from classic Americanos to innovative cold brew creations. We don't stop at coffee; we also offer an exquisite selection of teas, hot chocolates, and refreshing beverages for those looking for something different.
-
-      Cozy Ambiance
-
-      Our coffee shop is more than just a place to grab a cup of joe; it's a haven where you can unwind, work, or catch up with friends. Our welcoming atmosphere is designed to make you feel right at home. Enjoy the aroma of freshly roasted beans, soft jazz in the background, and comfortable seating that invites you to stay a little longer.
-
-      Local and Sustainable
-
-      We take pride in our commitment to the environment and our community. We source our coffee beans ethically and support local farmers whenever possible. Our commitment to sustainability extends to our eco-friendly cups and utensils, reducing our carbon footprint one sip at a time.
-
-      Homemade Treats
-
-      Pair your coffee with our delectable homemade pastries and snacks. From flaky croissants to indulgent cakes, our bakery selection is the perfect complement to your coffee experience.
-
-      Order with Ease
-
-      We value your time, which is why we offer convenient ordering options. You can place your order in advance through our mobile app for quick pickup or order from our friendly staff at the counter. Your coffee is prepared with care and ready when you are.
-
-      Community Hub
-
-      Mitho Coffee isn't just a place to grab a drink; it's a community hub. We host events, open mic nights, and art exhibitions, bringing people together to share their passions and creativity.
-
-      Visit Us Today
-
-      Experience the best coffee in town at Mitho Coffee. Whether you're a coffee connoisseur or simply looking for a welcoming spot to enjoy a cup, we invite you to join us. Our mission is to bring the joy of exceptional coffee to your day, one sip at a time.
+      Welcome to Mitho Coffee - Where Coffee Meets Perfection! At Mitho Coffee, we are passionate about crafting the perfect cup of coffee. We believe that every coffee lover deserves an exceptional coffee experience, and that's exactly what we offer. Step into our cozy coffee haven and embark on a journey of rich flavors, warm ambiance, and friendly service. The Art of Coffee We take our coffee seriously. Our skilled baristas are artists in their own right, expertly brewing each cup to perfection. Whether you prefer the bold intensity of an espresso or the smooth comfort of a latte, we've got you covered. Our coffee menu is a symphony of flavors, featuring single-origin beans sourced from around the world and roasted to perfection in-house. </p><br /><img src={shop1} alt="" /> <br /><br /> <p>A World of Variety At Mitho Coffee, we celebrate diversity. Our menu offers a wide range of coffee styles, from classic Americanos to innovative cold brew creations. We don't stop at coffee; we also offer an exquisite selection of teas, hot chocolates, and refreshing beverages for those looking for something different. Cozy Ambiance Our coffee shop is more than just a place to grab a cup of joe; it's a haven where you can unwind, work, or catch up with friends. Our welcoming atmosphere is designed to make you feel right at home. Enjoy the aroma of freshly roasted beans, soft jazz in the background, and comfortable seating that invites you to stay a little longer. </p><br /><img src={shop2} alt="" /><br /><br /><p>
+       Local and Sustainable We take pride in our commitment to the environment and our community. We source our coffee beans ethically and support local farmers whenever possible. Our commitment to sustainability extends to our eco-friendly cups and utensils, reducing our carbon footprint one sip at a time. Homemade Treats Pair your coffee with our delectable homemade pastries and snacks. From flaky croissants to indulgent cakes, our bakery selection is the perfect complement to your coffee experience. Order with Ease We value your time, which is why we offer convenient ordering options.</p><br /><img src={shop3} alt="" /> <br /><br /> You can place your order in advance through our mobile app for quick pickup or order from our friendly staff at the counter. Your coffee is prepared with care and ready when you are. Community Hub Mitho Coffee isn't just a place to grab a drink; it's a community hub. We host events, open mic nights, and art exhibitions, bringing people together to share their passions and creativity. Visit Us Today Experience the best coffee in town at Mitho Coffee. Whether you're a coffee connoisseur or simply looking for a welcoming spot to enjoy a cup, we invite you to join us. Our mission is to bring the joy of exceptional coffee to your day, one sip at a time.
 
       Stop by Mitho Coffee and discover why we're the talk of the town. Your perfect coffee moment awaits!
-      </p>
+      
     </div>
   )
 }
